4da2892037e84dbf6f8724798098156c7174dfde
Baptiste <baptiste.tougouri@epitech.eu>
2021-10-12 20:51:22 +0200

    Adding ListTiles and showing modal on item tap

--- lib/screens/dog_list.dart
+++ lib/screens/dog_list.dart
@@ -12,8 +12,25 @@ class DogList extends StatelessWidget {
       child: ListView.builder(
         itemCount: dogs.dogList.length,
           itemBuilder: (context, index) {
+          DogItem currentItem = dogs.dogList[index];
             return Container(
-              child: Text(dogs.dogList[index].name),
+              child: ListTile(
+                leading: Image.network(currentItem.imageUrl),
+                title: Text(currentItem.name),
+                onTap: () {
+                  showModalBottomSheet(context: context, builder: (BuildContext context) {
+                    return Container(
+                      child: Column(
+                        children: [
+                          Image.network(currentItem.imageUrl),
+                          Text(currentItem.name),
+                          Text(currentItem.description),
+                        ],
+                      ),
+                    );
+                  });
+                },
+              )
             );
           },
       ),
